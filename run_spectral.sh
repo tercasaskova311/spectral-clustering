3f06852f2ebb9e4592c5e77b478ee426ba59b2d7
Sásková <tereza.saskova@hpc-head-n2.unitn.it>
2026-01-11 11:24:39 +0100

    debug run_spectral.sh

--- run_spectral.sh
+++ run_spectral.sh
@@ -15,6 +15,7 @@ cd $PBS_O_WORKDIR
 module load mpich-3.2
 
 #initialize csv
+mkdir -p output
 echo "dataset,n,clusters,procs,t_load,t_degree,t_laplacian,t_eigen,t_kmeans,total" > output/timing.csv
 
 mpirun.actual -np 16 ./spectral_mpi
